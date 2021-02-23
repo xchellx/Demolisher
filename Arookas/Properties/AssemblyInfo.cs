a8d58a9abea9cd4db518cb074ca85760a29f9633
xchellx <jon.xjonx.nat@gmail.com>
2021-02-22 23:31:47 -0500

    Replace decompiled code with vs2010 output instead, cause .net 4.0 and all

---
+++ Arookas/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyTitle("Arookas Library")]
+[assembly: AssemblyDescription("Library containing common classes that I've made over the years which I find useful and use all the time.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Arookas")]
+[assembly: AssemblyProduct("Arookas Library")]
+[assembly: AssemblyCopyright("Copyright ©  2014 Arookas")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("51531489-0ca0-4ad8-bcc9-5d17927ac04b")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
